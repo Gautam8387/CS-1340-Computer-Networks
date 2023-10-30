c25b833ca9957900c4bb805161e6d0702548316e
gautam8387 <goutamahuja8387@gmail.com>
2023-10-31 03:26:32 +0530

    Linked-Layer Files Added -- Character Stuffing

---
+++ Link-Layer/character-stuffing.c
@@ -0,0 +1,13 @@
+// Character Stuffing Encoding for Link Layer of Computer Networks
+// Three Flags: STD, ETD, DLE
+// Original Data: char data[100]
+// Stuffed Data: char stuffed_data[200]
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char std, etd, dle;
+    return 0;
+}
